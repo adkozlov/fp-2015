b927a60ca18b7ce3ac883b1911d5be418bd19ab1
Andrew Kozlov <adkozlov@gmail.com>
2015-03-06 03:31:16 +0300

    hw 3 fixes

--- hw03/sort.hs
+++ hw03/sort.hs
@@ -1,9 +1,15 @@
-insert :: Int -> [Int] -> [Int]
-insert x [] = [x]
-insert x (y:ys) = if x <= y
-	then (x:y:ys)
-	else y:(insert x ys)
+rec :: a -> (Integer -> a -> a) -> Integer -> a
+rec z s 0 = z
+rec z s n = s (n - 1) $ rec z s $ n - 1
 
-sort :: [Int] -> [Int]
-sort [] = []
-sort (x:xs) = insert x (sort xs)
+recList :: a -> (b -> [b] -> a -> a) -> [b] -> a
+recList n c [] = n
+recList n c (x:xs) = c x xs $ recList n c xs
+
+insert :: Ord a => a -> [a] -> [a] -> [a]
+insert x _ [] = [x]
+insert x _ (y:ys) | x <= y = (x:y:ys)
+                  | otherwise = y:(insert x [] ys)
+
+sort :: Ord a => [a] -> [a]
+sort = recList [] insert
