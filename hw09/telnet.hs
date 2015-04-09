208d7c3bb0f619bb84966301744fd3b52d8d722a
valis <valery.isaev@gmail.com>
2015-04-09 15:02:19 +0300

    Add hw09

---
+++ hw09/telnet.hs
@@ -0,0 +1,11 @@
+import Network
+import Control.Concurrent(forkIO)
+
+{-
+Реализуйте простой телнет-клиент.
+Это программа, которая коннектится по указанному порту и отправляет весь ввод с stdin, а ответ выводит в stdout.
+(2 балла)
+-}
+
+main :: IO ()
+main = undefined
